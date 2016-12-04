6b0727adb48144314df65d5a39bf4853db8dbffe
Stijn Goethals <stijn_goethals@hotmail.com>
2016-12-04 16:40:58 +0100

    Update

--- web.py
+++ web.py
@@ -11,9 +11,29 @@ form = cgi.FieldStorage()
 
 
 over = Overschrijving()
-over.set_waarde(datum=form["datum"], bedrag=form["bedrag"], cent=form["cent"], r_opdracht=form["r_opdracht"],
-                i_opdracht=form["i_opdracht"], r_besch=form["r_besch"], b_besch=form["b_besch"],
-                i_besch=form["i_besch"], mededeling=form["mededeling"])
+datum = form["datum"].value
+bedrag = form["bedrag"].value
+cent = form["cent"].value
+r_opdracht = form["r_opdracht"].value
+i_opdracht = form["i_opdracht"].value
+r_besch = form["r_besch"].value
+b_besch = form["b_besch"].value
+i_besch = form["i_besch"].value
+mededeling = form["mededeling"].value
+
+print("Content-Type: text/html;charset=utf-8\n\n\n\n")
+
+print(datum)
+print(bedrag)
+print(cent)
+print(r_opdracht)
+print(i_opdracht)
+print(r_besch)
+print(b_besch)
+print(i_besch)
+print(mededeling)
+over.set_waarde(datum=datum, bedrag=bedrag, cent=cent, r_opdracht=r_opdracht, i_opdracht=i_opdracht, r_besch=r_besch,
+                b_besch=b_besch, i_besch=i_besch, mededeling=mededeling)
 over.schrijf()
 imgByteArr = io.BytesIO()
 over.get_image().save(imgByteArr, format='PNG')
@@ -21,5 +41,4 @@ imgByteArr = imgByteArr.getvalue()
 data_uri = base64.b64encode(imgByteArr).decode('utf-8').replace('\n', '')
 img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
 
-print("Content-Type: text/html;charset=utf-8")
 print(img_tag)
