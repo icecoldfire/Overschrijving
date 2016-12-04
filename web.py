68b49dbee074d529751875f983a07d6001f6218e
Stijn Goethals <stijn_goethals@hotmail.com>
2016-12-04 17:06:15 +0100

    Web page

--- web.py
+++ web.py
@@ -21,17 +21,6 @@ b_besch = form["b_besch"].value
 i_besch = form["i_besch"].value
 mededeling = form["mededeling"].value
 
-print("Content-Type: text/html;charset=utf-8\n\n\n\n")
-
-print(datum)
-print(bedrag)
-print(cent)
-print(r_opdracht)
-print(i_opdracht)
-print(r_besch)
-print(b_besch)
-print(i_besch)
-print(mededeling)
 over.set_waarde(datum=datum, bedrag=bedrag, cent=cent, r_opdracht=r_opdracht, i_opdracht=i_opdracht, r_besch=r_besch,
                 b_besch=b_besch, i_besch=i_besch, mededeling=mededeling)
 over.schrijf()
@@ -40,5 +29,5 @@ over.get_image().save(imgByteArr, format='PNG')
 imgByteArr = imgByteArr.getvalue()
 data_uri = base64.b64encode(imgByteArr).decode('utf-8').replace('\n', '')
 img_tag = '<img src="data:image/png;base64,{0}">'.format(data_uri)
-
+print("Content-Type: text/html;charset=utf-8\n\n\n\n")
 print(img_tag)
